ad40b6e4975d81471baa474c7435ffa8422c807f
Kizzzy <xuhua0174@163.com>
2023-10-10 11:37:50 +0800

    [http] * update JsonResponseParser

--- src/main/java/cn/kizzzy/http/parser/JsonResponseParser.java
+++ src/main/java/cn/kizzzy/http/parser/JsonResponseParser.java
@@ -1,25 +1,22 @@
 package cn.kizzzy.http.parser;
 
-import cn.kizzzy.helper.ClassHelper;
 import cn.kizzzy.http.HttpResponse;
 import cn.kizzzy.http.HttpResponseParser;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 
 import java.io.InputStream;
-import java.lang.reflect.Type;
 
-public abstract class JsonResponseParser<T> implements HttpResponseParser<T> {
-    
-    private final Type trueType;
-    
-    public JsonResponseParser() {
-        this.trueType = ClassHelper.getGenericClass(getClass());
-    }
+public abstract class JsonResponseParser<T> extends TypeReference<T> implements HttpResponseParser<T> {
     
     @Override
     public T parse(HttpResponse response) throws Exception {
         try (InputStream is = response.openInputStream()) {
-            return JSON.parseObject(is, trueType);
+            return JSON.parseObject(is, getType());
         }
+    }
+    
+    public T parse(String response) throws Exception {
+        return JSON.parseObject(response, getType());
     }
 }
